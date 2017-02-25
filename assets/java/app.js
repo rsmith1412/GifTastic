918e2a3b32a6f9b728f8e0feddd6bb4a6e04586d
Robert Smith <robertsmith1@Roberts-MacBook-Pro.local>
2017-02-25 02:52:15 -0800

    update

--- assets/java/app.js
+++ assets/java/app.js
@@ -1,34 +1,34 @@
 $(document).ready(function() {
 //Create array of strings, related to topic (comedians)
-var topics = ["Dave Chappelle", "Richard Pryor", "Dane Cook", "Robin Williams", "Katt Williams",
-	"Eddie Griffin", "Kevin Hart"
-]
-
-//var comedian = 
+var topics = ["tiger", "cheetah", "giraffe", "cat", "dog",
+	"shark", "turtle", "cow"];
 
 //App should take topics from array and create buttons in the HTML
-var newButton = $("<button>");
 
 //Create a for loop that appends a button for each string in array
 for (var i = 0; i < topics.length; i++) {
-	newButton, {
-		text: topics[i],
-		class: comics
-		click: laugh();
-		$("#comedians").append(newButton);
-	}
-	/*
-	var comic = newButton.append(topics[i]);
-	comic.innerHTML = topics[i];
-	comic.attr("value", topics[i]);*/
 
-}
-/*
+	console.log(topics[i]);
+	//Creates button element
+	var newButton = $("<button>");
+	//Adds class to button element
+	newButton.addClass("moo-button");
+	//Adds data-name to button element
+	newButton.attr("data-name", topics[i]);
+	//Adds name of comedian to button element
+	newButton.text(topics[i]);
+	//Takes topic from array and creates button in the HTML
+	$("#animal-buttons-div").append(newButton);
+
+}	
+
 //When user clicks on button, 10 static, non-animated gif images from GIPHY API will be shown on page
-$("#newButton").on("click", function() {
+$("button").on("click", function() {
+
+	var animal = $(this).attr("data-name");
 
 	//Creates a variable called queryURL that assigns this link
-	var queryURL = "http://api.giphy.com/v1/gifs/q=" + person + "?api_key=dc6zaTOxFJmzC";
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10&rating=pg";
 
 	$.ajax({
         url: queryURL,
@@ -43,24 +43,32 @@ $("#newButton").on("click", function() {
 
           var rating = results[i].rating;
 
+          console.log(results[i].images.fixed_height.url);
+
+          //Under every gif the rating of the gif will be displayed
           var p = $("<p>").text("Rating: " + rating);
 
-          var personImage = $("<img>");
-          personImage.attr("src", results[i].images.fixed_height.url);
+          var animalImage = $("<img>");
+          animalImage.attr("src", results[i].images.fixed_height.url);
+          animalImage.attr("data-state", "still");
+
 
           gifDiv.prepend(p);
-          gifDiv.prepend(personImage);
+          gifDiv.prepend(animalImage);
 
-          $("#comedians").prepend(gifDiv);
+          $("#zoo").prepend(gifDiv);  
         }
     });
+
 });
+
 
 //When user clicks on GIPHY still, gif should animate. User clicks again, gif stops
 
-//Under every gif the rating of the gif will be displayed
-
 //Add form that takes user input , and adds to topics array.
+/*$("#search").on('click', function() {
 
-//function call that takes each topic in array, and remakes the buttons on the page 
-});
+		var qID = $('#searchTerm').val();
+
+//function call that takes each topic in array, and remakes the buttons on the page */
+})
